e9877f9424a3b8619f16e97264bf7a1e1c6673f7
alexhenriquepv <alexhenriquepv@gmail.com>
2018-01-14 11:45:11 -0400

    Adicionado o body parser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const load = require('express-load')
+const bodyParser = require('body-parser')
 const app = express()
 
 app.use(express.static('public'))
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 load('config')
 	.then('dao')
